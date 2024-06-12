cb23d8355a0fa6029e486f4b6639243a2683bf1d
Arthur Prange <arthur.prange@gmail.com>
2024-06-12 12:36:28 -0300

    implementing middlewares

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 
 
@@ -7,18 +8,9 @@ const users =[];
 const server = http.createServer(async (req, res) => {
     const { method, url} = req
 
-    const buffers =[] 
+   await json(req, res)
 
-    for await (const chunk of req){
-        buffers.push(chunk)
-    }
 
-try{
-    req.body = JSON.parse(Buffer.concat(buffers).toString()) 
-} catch{
-        req.body = null;
-
-}
 
 
   
